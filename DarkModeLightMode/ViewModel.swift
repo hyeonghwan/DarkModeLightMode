7dcfce3e3d3a2c4a223048bd3cd50285f2cff3df
Hwan <ParkHyeongHwan@gmail.com>
2022-09-21 23:42:53 +0900

    viewModel add

--- DarkModeLightMode/ViewModel.swift
+++ DarkModeLightMode/ViewModel.swift
@@ -6,3 +6,45 @@
 //
 
 import Foundation
+import UIKit
+
+
+final class ViewModel {
+    
+    var lightMode: Mode = Mode.light
+    var darkMode: Mode = Mode.dark
+    
+    var darkImage = UIImage(named: "image_dark_mode")!
+    var lightImage = UIImage(named: "image_light_mode")!
+    
+    
+    // 데이터를 받아오면 뷰에게 이벤트 전달 -> 뷰 업데이트
+    var modeValue: ModeValue = .lightValue {
+        didSet{
+            
+            switch self.modeValue {
+                
+            case .lightValue:
+
+                lightModeReloadView?(true)
+                darkModeReloadView?(false)
+                
+            case .darkValue:
+                
+                darkModeReloadView?(true)
+                lightModeReloadView?(false)
+            }
+        }
+    }
+    
+    var darkModeReloadView: ((Bool) -> Void)? {
+        didSet{
+            print("darkModeReloadView didset")
+        }
+    }
+    var lightModeReloadView: ((Bool) -> Void)? {
+        didSet{
+            print("lightModeReloadView didset")
+        }
+    }
+}
