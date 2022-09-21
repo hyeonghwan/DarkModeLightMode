669e62a97c5f6f68a5d75d382a8fbcd178bed63d
Hwan <ParkHyeongHwan@gmail.com>
2022-09-21 23:44:13 +0900

    custom View add

--- DarkModeLightMode/SelectButton.swift
+++ DarkModeLightMode/SelectButton.swift
@@ -6,3 +6,38 @@
 //
 
 import Foundation
+import UIKit
+import SnapKit
+
+
+class SelectButton: UIButton {
+    private let selectedColor = UIColor(red: 0.263, green: 0.61, blue: 0.988, alpha: 1).cgColor
+    private let normalColor = UIColor(red: 0.855, green: 0.859, blue: 0.871, alpha: 1).cgColor
+    
+    override var isSelected: Bool {
+        didSet {
+            self.layer.borderColor =
+            self.isSelected ? selectedColor : normalColor
+        }
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+        
+    }
+    
+    private func configure() {
+        self.frame = CGRect(x: 0, y: 0, width: 30, height: 30)
+        self.backgroundColor = .white
+        self.layer.cornerRadius = self.frame.height / 2
+        self.clipsToBounds = true
+        self.layer.borderWidth = 2
+        self.layer.borderColor = normalColor
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("error")
+    }
+    
+}
